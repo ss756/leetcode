6dbd675f1868e2245284e93710f30f98fd524944
ss756 <ss756@snu.edu.in>
2020-05-24 02:20:48 +0530

    reverse the integer

---
+++ debug.py
@@ -0,0 +1,31 @@
+class Solution:
+        #     def reverse(self, x: int) -> int:
+        #         result = 0
+        #         abs_x = abs(x)
+        #         while abs_x != 0 :
+        #             remainder = abs_x % 10
+        #             abs_x = abs_x //10
+
+        #             temp = result * 10 + remainder
+        #             result = temp
+
+        #         if x < 0 :
+        #             result = -1 * result
+
+        #         if result < - 2**31 or result > 2**31 -1:
+        #             result = 0
+
+        #         return result
+        def reverse(self, x: int) -> int:
+            absx = str(abs(x))
+            reversed = int(absx[::-1])
+            if x < 0:
+                reversed *= -1
+            if reversed > 2 ** 31 - 1 or reversed < -2 ** 31:
+                return 0
+            return reversed
+
+        def __init__(self):
+                print(self.reverse(15342364969))
+
+Solution()
