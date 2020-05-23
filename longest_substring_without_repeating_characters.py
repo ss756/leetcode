aec6f3d5eb026accfa2c914ff5487c1961191509
ss756 <ss756@snu.edu.in>
2020-05-24 00:49:06 +0530

    first approach to zog zag problem

--- longest_substring_without_repeating_characters.py
+++ longest_substring_without_repeating_characters.py
@@ -1,3 +1,15 @@
+'''
+    Problem Statement:
+    Given a string, find the length of the longest substring without repeating characters.
+
+
+    Input: "abcabcbb"
+    Output: 3
+    Explanation: The answer is "abc", with the length of 3.
+
+'''
+
+
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         substr:str=''
---
+++ zigzag.py
@@ -0,0 +1,49 @@
+'''
+    Problem Statement:
+    The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+    P   A   H   N
+    A P L S I I G
+    Y   I   R
+
+    And then read line by line: "PAHNAPLSIIGYIR"
+
+    Input: s = "PAYPALISHIRING", numRows = 4
+    Output: "PINALSIGYAHRPI"
+    Explanation:
+
+    P     I    N
+    A   L S  I G
+    Y A   H R
+    P     I
+'''
+
+
+# approach number one (execution time is not good this can be optimised)
+
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        row=1
+        character_pos= list(range(len(s)))
+        final_str=''
+        for i in range(0, len(s)):
+            if numRows == 1:
+                return s
+            if row == numRows:
+                down_movement = False
+            elif row == 1:
+                down_movement = True
+            if down_movement == True:
+                character_pos[i]=row
+                row+=1
+            if down_movement == False:
+                character_pos[i]=row
+                row-=1
+        for i in range(1,numRows+1):
+            row_str=''
+            for j in range(len(character_pos)):
+                if character_pos[j]==i:
+                    row_str+= s[j]
+            final_str+=row_str
+            row_str=''
+        return final_str
