492715607f77d6e48728a754d946f91cd78d75ce
ss756 <ss756@snu.edu.in>
2020-06-01 17:14:56 +0530

    all the string permutations

--- string_permutation.py
+++ string_permutation.py
@@ -5,8 +5,28 @@ def permute_string_method_one(s: str, prefix=''):
     for k in range(0, len(s)):
         permute_string_method_one(s[0:k]+s[k+1:], prefix + s[k])
 
+def toString(a):
+    return ''.join(a)
+
+
+def permute_string_method_two(s, l: int, r: int):
+    if l == r:
+        print(toString(s))
+        return
+    else:
+        for i in range(l, r+1):
+            s[l], s[i] = s[i], s[l]
+            print(i, l, s)
+            permute_string_method_two(s, l+1, r)
+            s[l], s[i] = s[i], s[l]
+
 
 if __name__ == "__main__":
-    s = "abc"
-    permute_string_method_one(s)
+    s = "abcde"
+#    permute_string_method_one(s)
+    string = "abc"
+    n = len(string)
+    a = list(string)
+    permute_string_method_two(a, 0, n - 1)
+
 
