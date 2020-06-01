6818bed1ff653b58f7f4dfc7c88359a9c9b11efd
ss756 <ss756@snu.edu.in>
2020-06-01 17:18:35 +0530

    added new problems

--- all_combinations_that_add_to_a_number.py
+++ all_combinations_that_add_to_a_number.py
@@ -7,10 +7,9 @@ def subset_sum(numbers, target, partial=[]):
     for i in range(0, len(numbers)):
         n = numbers[i]
         remaining = numbers[i+1:]
-        print(partial, remaining, target)
         subset_sum(remaining, target, [n] + partial)
 
-if __name__ == "__main__":
+if  __name__=="__main__":
     numbers= [3, 5, 7, 9, 8, 12, -5, 25]
     target = 20
     subset_sum(numbers, target)
