f6101214850b23664a9c2655fbe276620976dff2
ss756 <ss756@snu.edu.in>
2020-06-11 00:43:51 +0530

    decoded string

--- decodestring.py
+++ decodestring.py
@@ -1,6 +1,27 @@
 class Solution:
-    def decodestring(self, s:str):
+    def decodestring(self, s: str) -> str:
+        stack = []
+        curnum = 0
+        curstr = ''
+        for k in s:
+            if k.isdigit():
+                curnum = curnum * 10 + int(k)
+            elif k == '[':
+                stack.append(curstr)
+                stack.append(curnum)
+                curnum = 0
+                curstr = ''
+            elif k == ']':
+                num = stack.pop()
+                prevstring = stack.pop()
+                curstr = prevstring + curstr * num
+            else:
+                curstr += k
+        return curstr
 
+    def __init__(self):
+        s = "3[a2[c]]"
+        print(self.decodestring(s))
 
 
 Solution()
